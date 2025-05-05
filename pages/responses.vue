fa5cb55c7a0f3da92da4f666bd81595ba99d46f8
AnatarX <flameelite88@mail.ru>
2025-05-05 15:00:55 +0300

    add responses[old]

--- pages/responses.vue
+++ pages/responses.vue
@@ -20,10 +20,54 @@ useHead({
 <template>
     <Header :lang="lang" :active_nav_section="null" />
     <div id="content-container">
-        <p>Lorem Ipsum</p>
+        <div id="content-block">
+            <div class="card-item">
+                <div class="card-title">
+                    <img src="~/public/yandex.png">
+                    <label>Яндекс</label>
+                </div>
+                <div class="card-description">
+                    <label class="card-description-name">ML-разработчик</label>
+                    <label class="card-description-short-description">В музыкальный поиск Алисы</label>
+                </div>
+                <div class="card-status">
+                    <p>Статус:</p>
+                    <p class="card-status-value-ok">Одобрено</p>
+                </div>
+            </div>
+            <div class="card-item">
+                <div class="card-title">
+                    <img src="~/public/yandex.png">
+                    <label>Яндекс</label>
+                </div>
+                <div class="card-description">
+                    <label class="card-description-name">ML-разработчик</label>
+                    <label class="card-description-short-description">В музыкальный поиск Алисы</label>
+                </div>
+                <div class="card-status">
+                    <p>Статус:</p>
+                    <p class="card-status-value-pending">На рассмотрении</p>
+                </div>
+            </div>
+            <div class="card-item">
+                <div class="card-title">
+                    <img src="~/public/yandex.png">
+                    <label>Яндекс</label>
+                </div>
+                <div class="card-description">
+                    <label class="card-description-name">ML-разработчик</label>
+                    <label class="card-description-short-description">В музыкальный поиск Алисы</label>
+                </div>
+                <div class="card-status">
+                    <p>Статус:</p>
+                    <p class="card-status-value-reject">Отказано</p>
+                </div>
+            </div>
+        </div>
     </div>
     <Footer :lang="lang"/>
 </template>
 
 <style>
+@import url('~/public/css/responses.css');
 </style>
